6afe9870a74d3bbd0deedbb5bacebf391c7a0cae
esmaeelE <esmaeelEE@protonmail.com>
2023-02-24 19:21:08 +0330

    first session

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python_teach
 My dump repository for teaching how to code
+
+You can use it under [GPL](gpl-3.0.txt) license
+
---
+++ codes/1.py
@@ -0,0 +1,19 @@
+# # mashin hesab
+
+# gamme aval : khandane voroudi
+# str : string رشته
+
+first_number = int(input("adade aval ra vard kon: "))
+second_number = int(input("adade dovom ra vard kon: "))
+
+# # gamme dovom : mohased
+jam = first_number + second_number
+tafrig = first_number - second_number
+zarb = first_number * second_number
+tagsim = first_number / second_number
+
+# gamme sevom : chaap
+print(f"{first_number} + {second_number} = {jam}")
+print(f"{first_number} - {second_number} = {tafrig}")
+print(f"{first_number} * {second_number} = {zarb}")
+print(f"{first_number} / {second_number} = {tagsim}")
